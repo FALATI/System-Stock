fb420156d531a1a291be9902d3334fc9fae595f3
jeferson <jeferson@jeferson-pc.domain.name>
2017-10-17 07:34:36 -0200

    Interface

--- includes/header.php
+++ includes/header.php
@@ -36,7 +36,7 @@
 <body>
 
 
-	<nav class="navbar navbar-default navbar-static-top">
+	<nav class="navbar navbar-default navbar-fixed-top">
 		<div class="container">
     <!-- Brand and toggle get grouped for better mobile display -->
     <div class="navbar-header">
