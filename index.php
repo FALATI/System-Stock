e727f0bc601a4a11df5c06e9e08f0544ef1093a7
falati <jeferson.falati@gmail.com>
2019-01-06 01:06:51 -0200

    modificado url para acesso

--- index.php
+++ index.php
@@ -1,395 +1,139 @@
-<!--A Design by W3layouts
-Author: W3layout
-Author URL: http://w3layouts.com
-License: Creative Commons Attribution 3.0 Unported
-License URL: http://creativecommons.org/licenses/by/3.0/
--->
-<!DOCTYPE HTML>
+<?php 
+require_once 'php_action/db_connect.php';
+
+session_start();
+
+if(isset($_SESSION['userId'])) {
+	header('location: http://localhost/stock/dashboard.php');	
+}
+
+$errors = array();
+
+if($_POST) {		
+
+	$username = $_POST['username'];
+	$password = $_POST['password'];
+
+	if(empty($username) || empty($password)) {
+		if($username == "") {
+			$errors[] = "Username is required";
+		} 
+
+		if($password == "") {
+			$errors[] = "Password is required";
+		}
+	} else {
+		$sql = "SELECT * FROM users WHERE username = '$username'";
+		$result = $connect->query($sql);
+
+		if($result->num_rows == 1) {
+			$password = md5($password);
+			// exists
+			$mainSql = "SELECT * FROM users WHERE username = '$username' AND password = '$password'";
+			$mainResult = $connect->query($mainSql);
+
+			if($mainResult->num_rows == 1) {
+				$value = $mainResult->fetch_assoc();
+				$user_id = $value['user_id'];
+
+				// set session
+				$_SESSION['userId'] = $user_id;
+
+				header('location: http://localhost/stock/dashboard.php');	
+			} else{
+				
+				$errors[] = "Incorrect username/password combination";
+			} // /else
+		} else {		
+			$errors[] = "Username doesnot exists";		
+		} // /else
+	} // /else not empty username // password
+	
+} // /if $_POST
+?>
+
+<!DOCTYPE html>
 <html>
 <head>
-<title>Stock</title>
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<meta name="keywords" content="Lookz Responsive web template, Bootstrap Web Templates, Flat Web Templates, Andriod Compatible web template, 
-Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyErricsson, Motorola web design" />
-<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
-<link href="css/bootstrap.css" rel='stylesheet' type='text/css' />
-<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-<!-- Custom Theme files -->
-<link href="css/style.css" rel='stylesheet' type='text/css' />
-<!-- Custom Theme files -->
-<!--webfont-->
-<link href='//fonts.googleapis.com/css?family=Raleway:100,200,300,400,500,600,700,800,900' rel='stylesheet' type='text/css'>
-<script type="text/javascript" src="js/jquery-1.11.1.min.js"></script>
-<!-- start menu -->
-<link href="css/megamenu.css" rel="stylesheet" type="text/css" media="all" />
-<script type="text/javascript" src="js/megamenu.js"></script>
-<script>$(document).ready(function(){$(".megamenu").megamenu();});</script>
+	<title>Stock Management System</title>
+
+	<!-- bootstrap -->
+	<link rel="stylesheet" href="assests/bootstrap/css/bootstrap.min.css">
+	<!-- bootstrap theme-->
+	<link rel="stylesheet" href="assests/bootstrap/css/bootstrap-theme.min.css">
+	<!-- font awesome -->
+	<link rel="stylesheet" href="assests/font-awesome/css/font-awesome.min.css">
+
+  <!-- custom css -->
+  <link rel="stylesheet" href="custom/css/custom.css">	
+
+  <!-- jquery -->
+	<script src="assests/jquery/jquery.min.js"></script>
+  <!-- jquery ui -->  
+  <link rel="stylesheet" href="assests/jquery-ui/jquery-ui.min.css">
+  <script src="assests/jquery-ui/jquery-ui.min.js"></script>
+
+  <!-- bootstrap js -->
+	<script src="assests/bootstrap/js/bootstrap.min.js"></script>
 </head>
 <body>
-<div class="wrap">	
-   <div class="container">
-        <div class="header_top">
-		  <div class="col-sm-9 h_menu4">
-				<ul class="megamenu skyblue">
-					  <li class="active grid"><a class="color8" href="index.php">New</a></li>	
-				      <li><a class="color1" href="#">Men</a><div class="megapanel">
-						<div class="row">
-							<div class="col1">
-								<div class="h_nav">
-									<ul>
-										<li><a href="men.html">Accessories</a></li>
-										<li><a href="men.html">Bags</a></li>
-										<li><a href="men.html">Caps & Hats</a></li>
-										<li><a href="men.html">Hoodies & Sweatshirts</a></li>
-										<li><a href="men.html">Jackets & Coats</a></li>
-										<li><a href="men.html">Jeans</a></li>
-										<li><a href="men.html">Jewellery</a></li>
-										<li><a href="men.html">Jumpers & Cardigans</a></li>
-										<li><a href="men.html">Leather Jackets</a></li>
-										<li><a href="men.html">Long Sleeve T-Shirts</a></li>
-										<li><a href="men.html">Loungewear</a></li>
-									</ul>	
-								</div>							
-							</div>
-							<div class="col1">
-								<div class="h_nav">
-									<ul>
-										<li><a href="men.html">Shirts</a></li>
-										<li><a href="men.html">Shoes, Boots & Trainers</a></li>
-										<li><a href="men.html">Shorts</a></li>
-										<li><a href="men.html">Suits & Blazers</a></li>
-										<li><a href="men.html">Sunglasses</a></li>
-										<li><a href="men.html">Sweatpants</a></li>
-										<li><a href="men.html">Swimwear</a></li>
-										<li><a href="men.html">Trousers & Chinos</a></li>
-										<li><a href="men.html">T-Shirts</a></li>
-										<li><a href="men.html">Underwear & Socks</a></li>
-										<li><a href="men.html">Vests</a></li>
-									</ul>	
-								</div>							
-							</div>
-							<div class="col1">
-								<div class="h_nav">
-									<h4>Popular Brands</h4>
-									<ul>
-										<li><a href="men.html">Levis</a></li>
-										<li><a href="men.html">Persol</a></li>
-										<li><a href="men.html">Nike</a></li>
-										<li><a href="men.html">Edwin</a></li>
-										<li><a href="men.html">New Balance</a></li>
-										<li><a href="men.html">Jack & Jones</a></li>
-										<li><a href="men.html">Paul Smith</a></li>
-										<li><a href="men.html">Ray-Ban</a></li>
-										<li><a href="men.html">Wood Wood</a></li>
-									</ul>	
-								</div>												
-							</div>
-						  </div>
-						</div>
-					</li>
-				    <li class="grid"><a class="color2" href="#">Women</a>
-					  <div class="megapanel">
-						<div class="row">
-							<div class="col1">
-								<div class="h_nav">
-									<ul>
-										<li><a href="men.html">Accessories</a></li>
-										<li><a href="men.html">Bags</a></li>
-										<li><a href="men.html">Caps & Hats</a></li>
-										<li><a href="men.html">Hoodies & Sweatshirts</a></li>
-										<li><a href="men.html">Jackets & Coats</a></li>
-										<li><a href="men.html">Jeans</a></li>
-										<li><a href="men.html">Jewellery</a></li>
-										<li><a href="men.html">Jumpers & Cardigans</a></li>
-										<li><a href="men.html">Leather Jackets</a></li>
-										<li><a href="men.html">Long Sleeve T-Shirts</a></li>
-										<li><a href="men.html">Loungewear</a></li>
-									</ul>	
-								</div>							
-							</div>
-							<div class="col1">
-								<div class="h_nav">
-									<ul>
-										<li><a href="men.html">Shirts</a></li>
-										<li><a href="men.html">Shoes, Boots & Trainers</a></li>
-										<li><a href="men.html">Shorts</a></li>
-										<li><a href="men.html">Suits & Blazers</a></li>
-										<li><a href="men.html">Sunglasses</a></li>
-										<li><a href="men.html">Sweatpants</a></li>
-										<li><a href="men.html">Swimwear</a></li>
-										<li><a href="men.html">Trousers & Chinos</a></li>
-										<li><a href="men.html">T-Shirts</a></li>
-										<li><a href="men.html">Underwear & Socks</a></li>
-										<li><a href="men.html">Vests</a></li>
-									</ul>	
-								</div>							
-							</div>
-							<div class="col1">
-								<div class="h_nav">
-									<h4>Popular Brands</h4>
-									<ul>
-										<li><a href="men.html">Levis</a></li>
-										<li><a href="men.html">Persol</a></li>
-										<li><a href="men.html">Nike</a></li>
-										<li><a href="men.html">Edwin</a></li>
-										<li><a href="men.html">New Balance</a></li>
-										<li><a href="men.html">Jack & Jones</a></li>
-										<li><a href="men.html">Paul Smith</a></li>
-										<li><a href="men.html">Ray-Ban</a></li>
-										<li><a href="men.html">Wood Wood</a></li>
-									</ul>	
-								</div>												
-							</div>
-						  </div>
-						</div>
-			    </li>
-				<li><a class="color4" href="404.html">Accessories</a></li>				
-				<li><a class="color6" href="contact.html">Contact</a></li>
-			  </ul> 
-			</div>
-			<div class="col-sm-3 header-top-right">
-			     <div class="drop_buttons">
-   				    <select class="drop-down drop-down-in">
-						<option value="1">English</option>
-						<option value="2">French</option>
-						<option value="3">Spanish</option>
-					</select>
-   			       <div class="clearfix"></div>
-   			    </div>
-   			    <div class="register-info">
-				    <ul>
-						<li><a href="login.php">Login</a></li>
-					</ul>
-			    </div>
-				<div class="clearfix"> </div>
-   			 </div>
-   			 <div class="clearfix"> </div>
-	</div>
-    <div class="header_bootm">
-		<div class="col-sm-4 span_1">
-		  <div class="logo">
-			<a href="index.html"><img src="admin/assests/images/logo-dark.png" alt="Stock"/></a>
-		  </div>
-		</div>
-		<div class="col-sm-8 row_2">
-		  <div class="header_bottom_right">
-			<div class="search">
-			  <input type="text" value="Your email address" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Your email address';}">
-			  <input type="submit" value="">
-	  		</div>
-	  		<ul class="bag">
-	  			<a href="cart.html"><i class="bag_left"> </i></a>
-	  			<a href="cart.html"><li class="bag_right"><p>205.00 $</p> </li></a>
-	  			<div class="clearfix"> </div>
-	  		</ul>
-	  		<div class="clearfix"> </div>
-		   </div>
-		</div>
-		 <div class="clearfix"></div>
-	</div>
-	<!--<div class="grid_1">
-		<div class="col-md-3 banner_left">
-			<img src="images/pic1.png" class="img-responsive" alt=""/>
-			<div class="banner_desc">
-				<h1>Slim Fit Men </h1>
-				<h2>Roundcheck T-Shirt</h2>
-				<h5>$125
-					   <small>Only</small>
-					</h5>
-			    <a href="#" class="btn1 btn4 btn-1 btn1-1b">Add To Cart</a>
-			</div>
-		</div>
-		<div class="col-md-9 banner_right">
-					<script src="js/responsiveslides.min.js"></script>
-								<script>
-									// You can also use "$(window).load(function() {"
-									$(function () {
-									 // Slideshow 4
-									$("#slider3").responsiveSlides({
-										auto: true,
-										pager: false,
-										nav: false,
-										speed: 500,
-										namespace: "callbacks",
-										before: function () {
-									$('.events').append("<li>before event fired.</li>");
-									},
-									after: function () {
-										$('.events').append("<li>after event fired.</li>");
-										}
-										});
-										});
-								</script>
-				<div  id="top" class="callbacks_container">
-					<ul class="rslides" id="slider3">
-						<li>
-							<img src="images/banner.jpg" alt=""/>
-						</li>
-						<li>
-							<img src="images/banner1.jpg" alt=""/>
-						</li>
-					</ul>
-				</div>
-		</div>
-		<div class="clearfix"></div>
-	</div>-->
-<div class="content">
-<!---728x90--->
-  <div class="content_box">
-	<ul class="grid_2">
-		<a href="single.html"><li><img src="images/pic2.png" class="img-responsive" alt=""/>
-			<span class="btn5">$120</span>
-			<p>Bikroy Tshirt - Roundneck</p>
-		</li></a>
-		<a href="single.html"><li><img src="images/pic3.png" class="img-responsive" alt=""/>
-			<span class="btn5">$340</span>
-			<p>Park Tshirt - Partygrandd</p>
-		</li></a>
-		<a href="single.html"><li><img src="images/pic4.png" class="img-responsive" alt=""/>
-			<span class="btn5">$250</span>
-			<p>Gray Tshirt Roundneckdd</p>
-		</li></a>
-		<a href="single.html"><li><img src="images/pic5.png" class="img-responsive" alt=""/>
-			<span class="btn5">$378</span>
-			<p>Marivo Tshirt - Roundneck</p>
-		</li></a>
-		<a href="single.html"><li class="last1"><img src="images/pic6.png" class="img-responsive" alt=""/>
-			<span class="btn5">$428</span>
-			<p>Strict TshirtSoft, Pure Cotton</p>
-		</li></a>
-		<div class="clearfix"> </div>
-	</ul>
-	<!---728x90--->
-	<div class="grid_3">
-		<div class="col-md-6 box_2">
-			<div class="section_group"> 
-		      	<div class="col_1_of_2 span_1_of_2">
-		      		<img src="images/pic7.jpg" class="img-responsive" alt=""/>
-		        </div>
-                <div class="col_1_of_2 span_1_of_2">
-		      		<img src="images/pic8.jpg" class="img-responsive" alt=""/>
-		        </div>
-                <div class="clearfix"> </div>
-            </div>
-		</div>
-		<div class="col-md-6">
-			<div class="box_3">
-			  <div class="col_1_of_2 span_1_of_2 span_3">
-		      		<h3>Paul Slim Fit Men
-						Roundneck
-						T-Shirt</h3>
-                  <h4>$156</h4>
-                  <p>Offer Available till Sunday 12 Nov 2014.</p>
-                  <a href="#" class="btn1 btn6 btn-1 btn1-1b">Add To Cart</a>
-		        </div>
-                <div class="col_1_of_2 span_1_of_2 span_4">
-                   <div class="span_5">
-		      		 <img src="images/pic9.png" class="img-responsive" alt=""/>
-		      	    </div>
-		        </div>
-                <div class="clearfix"> </div>
-            </div>
-		</div>
-		<div class="clearfix"> </div>
-	</div>
-	<!---728x90--->
-	<div class="footer_top">
-	  <div class="span_of_4">
-		<div class="col-md-3 box_4">
-			<h4>Shop</h4>
-			<ul class="f_nav">
-				<li><a href="#">new arrivals</a></li>
-				<li><a href="#">men</a></li>
-				<li><a href="#">women</a></li>
-				<li><a href="#">accessories</a></li>
-				<li><a href="#">kids</a></li>
-				<li><a href="#">brands</a></li>
-				<li><a href="#">trends</a></li>
-				<li><a href="#">sale</a></li>
-				<li><a href="#">style videos</a></li>
-			</ul>	
-		</div>
-		<div class="col-md-3 box_4">
-			<h4>help</h4>
-			<ul class="f_nav">
-				<li><a href="#">frequently asked  questions</a></li>
-				<li><a href="#">men</a></li>
-				<li><a href="#">women</a></li>
-				<li><a href="#">accessories</a></li>
-				<li><a href="#">kids</a></li>
-				<li><a href="#">brands</a></li>
-			</ul>				
-		</div>
-		<div class="col-md-3 box_4">
-			<h4>account</h4>
-			<ul class="f_nav">
-				<li><a href="#">login</a></li>
-				<li><a href="#">create an account</a></li>
-				<li><a href="#">create wishlist</a></li>
-				<li><a href="#">my shopping bag</a></li>
-				<li><a href="#">brands</a></li>
-				<li><a href="#">create wishlist</a></li>
-			</ul>
-		</div>
-		<div class="col-md-3 box_4">
-			<h4>popular</h4>
-			<ul class="f_nav">
-				<li><a href="#">new arrivals</a></li>
-				<li><a href="#">men</a></li>
-				<li><a href="#">women</a></li>
-				<li><a href="#">accessories</a></li>
-				<li><a href="#">kids</a></li>
-				<li><a href="#">brands</a></li>
-				<li><a href="#">trends</a></li>
-				<li><a href="#">sale</a></li>
-				<li><a href="#">style videos</a></li>
-				<li><a href="#">login</a></li>
-				<li><a href="#">brands</a></li>
-			</ul>			
-		</div>
-		<div class="clearfix"></div>
-	</div>
-		<!-- start span_of_2 -->
-		<div class="span_of_2">
-			<div class="span1_of_2">
-				<h5>need help? <a href="#">contact us <span> &gt;</span> </a> </h5>
-				<p>(or) Call us: +22-34-2458793</p>
-			</div>
-			<div class="span1_of_2">
-				<h5>follow us </h5>
-				<div class="social-icons">
-					     <ul>
-					        <li><a href="#" target="_blank"></a></li>
-					        <li><a href="#" target="_blank"></a></li>
-					        <li><a href="#" target="_blank"></a></li>
-					        <li><a href="#" target="_blank"></a></li>
-					        <li class="last2"><a href="#" target="_blank"></a></li>
-						</ul>
-				</div>
-			</div>
-			<div class="clearfix"></div>
-		</div>
-		<div class="copy">
-		   <p>© 2017 Fala Ti. All Rights Reserved.</p>
-		</div>
-     </div>
-   </div>
-</div>	
-<link href="css/flexslider.css" rel='stylesheet' type='text/css' />
-							  <script defer src="js/jquery.flexslider.js"></script>
-							  <script type="text/javascript">
-								$(function(){
-								  SyntaxHighlighter.all();
-								});
-								$(window).load(function(){
-								  $('.flexslider').flexslider({
-									animation: "slide",
-									start: function(slider){
-									  $('body').removeClass('loading');
+	<div class="container">
+		<div class="row vertical">
+			<div class="col-md-5 col-md-offset-4">
+				<div class="panel panel-info">
+					<div class="panel-heading">
+						<h3 class="panel-title">Please Sign in</h3>
+					</div>
+					<div class="panel-body">
+
+						<div class="messages">
+							<?php if($errors) {
+								foreach ($errors as $key => $value) {
+									echo '<div class="alert alert-warning" role="alert">
+									<i class="glyphicon glyphicon-exclamation-sign"></i>
+									'.$value.'</div>';										
 									}
-								  });
-								});
-							  </script>
+								} ?>
+						</div>
+
+						<form class="form-horizontal" action="<?php echo $_SERVER['PHP_SELF'] ?>" method="post" id="loginForm">
+							<fieldset>
+							  <div class="form-group">
+									<label for="username" class="col-sm-2 control-label">Username</label>
+									<div class="col-sm-10">
+									  <input type="text" class="form-control" id="username" name="username" placeholder="Username" autocomplete="off" />
+									</div>
+								</div>
+								<div class="form-group">
+									<label for="password" class="col-sm-2 control-label">Password</label>
+									<div class="col-sm-10">
+									  <input type="password" class="form-control" id="password" name="password" placeholder="Password" autocomplete="off" />
+									</div>
+								</div>								
+								<div class="form-group">
+									<div class="col-sm-offset-2 col-sm-10">
+									  <button type="submit" class="btn btn-default"> <i class="glyphicon glyphicon-log-in"></i> Sign in</button>
+									</div>
+								</div>
+							</fieldset>
+						</form>
+					</div>
+					<!-- panel-body -->
+				</div>
+				<!-- /panel -->
+			</div>
+			<!-- /col-md-4 -->
+		</div>
+		<!-- /row -->
+	</div>
+	<!-- container -->	
 </body>
-</html>		
+</html>
+
+
+
+
+
+
+
+	
